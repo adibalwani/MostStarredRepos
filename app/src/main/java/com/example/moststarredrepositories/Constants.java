b09ace22dd25c8b5d2d9d09b3c9915449a93056b
Adib Alwani <aalwani@vmware.com>
2016-06-09 11:26:57 -0700

    RecyclerView now displays the list of most starred Repos

--- app/src/main/java/com/example/moststarredrepositories/Constants.java
+++ app/src/main/java/com/example/moststarredrepositories/Constants.java
@@ -4,8 +4,19 @@ package com.example.moststarredrepositories;
  * Constants defined in the whole app
  */
 public class Constants {
-    // Search Repo URLs
+
+    // https://api.github.com/search/repositories?q=created:>2016-06-03&sort=stars&order=desc
+
+    // RecyclerView Span count
+    public static final int SPAN_COUNT_PORTRAIT = 2;
+
+    // Search Repo URLs and Params
     public static final String SEARCH_REPOS_URL = "https://api.github.com/search/repositories";
+    public static final String SEARCH_REPOS_QUERY_PARAM = "q";
+    public static final String SEARCH_REPOS_SORT_PARAM = "sort";
+    public static final String SEARCH_REPOS_SORT_PARAM_VALUE = "stars";
+    public static final String SEARCH_REPOS_ORDER_PARAM = "order";
+    public static final String SEARCH_REPOS_ORDER_PARAM_VALUE = "desc";
 
     // Error Messages
     public static final String INTERNET_UNAVAILABLE = "NO INTERNET CONNECTION";
---
+++ app/src/main/java/com/example/moststarredrepositories/RecyclerViewAdapter.java
@@ -0,0 +1,75 @@
+package com.example.moststarredrepositories;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Provide views to RecyclerView
+ */
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String LOG_TAG = RecyclerViewAdapter.class.getSimpleName();
+
+    private String[] mDataSet;
+    private Context mContext;
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
+     */
+    public RecyclerViewAdapter(String[] dataSet, Context context) {
+        mDataSet = dataSet;
+        mContext = context;
+    }
+
+    /**
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView repoName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            // Define click listener for the ViewHolder's View.
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Show Detail View/Activity
+                }
+            });
+            repoName = (TextView) itemView.findViewById(R.id.repo_name_text_view);
+        }
+
+        public TextView getRepoName() {
+            return repoName;
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Create a new view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_base, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    // Get element from the dataset at this position and replace the contents of the view
+    // with that element
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.getRepoName().setText(mDataSet[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+}
